e13bc6552455d030b2cd8cf6d1dc66c6b0f0c01a
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2016-03-22 15:47:05 +0100

    Register models in admin interface

--- timesheets/admin.py
+++ timesheets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, SubProject, Employee
+
+admin.site.register(Project)
+admin.site.register(SubProject)
+admin.site.register(Employee)
