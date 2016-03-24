945493d90fb5dc42efb326f4efbcdf66fedfef62
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-03-24 11:43:57 +0100

    Declare TimeRecord through admin interface

--- timesheets/admin.py
+++ timesheets/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, SubProject, Employee
+from .models import Project, SubProject, Employee, TimeRecord
 
 admin.site.register(Project)
 admin.site.register(SubProject)
 admin.site.register(Employee)
+admin.site.register(TimeRecord)
