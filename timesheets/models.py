5cc6da60c09fb55819490bf79ea2cd0b3e13e049
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-03-24 11:44:23 +0100

    Updated models

--- timesheets/models.py
+++ timesheets/models.py
@@ -1,23 +1,85 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+
+import math
 
 # Create your models here.
 
-class Project(models.Model):
-    name = models.CharField(max_length=30)
-    initials = models.CharField(max_length=5)
-
-class SubProject(models.Model):
-    name = models.CharField(max_length=30)
-    initials = models.CharField(max_length=5)
-    parent_project = models.ForeignKey(Project, blank=True, null=True)
 
 class Employee(models.Model):
-    user = models.OneToOneField(User, unique=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    color = models.CharField(max_length=7, default="#ffffff")
 
-class Timeline(models.Model):
-    employee = models.ForeignKey(Employee)
+    def __str__(self):
+        return '{}'.format(self.user.username)
+
+
+class TimeRecordManager(models.Manager):
+    def by_date(self, date):
+        return self.filter(date=date)
+
+    def hours_by_project_and_date(self, project, date):
+        total = 0
+        for hour in self.filter(project=project, date=date):
+            total += hour
+            return total
+
+
+class TimeRecord(models.Model):
+    employee = models.ForeignKey('Employee')
     date = models.DateField()
     hours = models.FloatField()
-    project = models.ForeignKey(Project)
-    
+    project = models.ForeignKey('SubProject')
+    objects = TimeRecordManager()
+
+    class Meta(object):
+        ordering = ('project', 'date', 'employee',)
+        unique_together = ('project', 'date', 'employee')
+
+    @property
+    def formatted_hours(self):
+        h = int(math.floor(self.hours))
+        m = int((self.hours - h) * 60)
+        return '{:d}h{:02d}'.format(h, m)
+
+    def __str__(self):
+        return '{!s} - {} - {!s}'.format(self.project, self.date, self.employee)
+
+
+class Project(models.Model):
+    initials = models.CharField(unique=True, max_length=5)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '{} - {}'.format(self.initials, self.name)
+
+
+class SubProjectManager(models.Manager):
+    pass
+
+    def timerecords_by_date(self, date):
+        return self.timerecords.filter(date=date).order_by('date')
+
+
+class SubProject(models.Model):
+    initials = models.CharField(max_length=5)
+    name = models.CharField(max_length=30)
+    parent_project = models.ForeignKey('Project', blank=True, null=True)
+    objects = SubProjectManager()
+
+    class Meta:
+        unique_together = ('initials', 'parent_project')
+
+    @property
+    def timerecords(self):
+        return TimeRecord.objects.filter(project=self)
+
+    def total_hours(self):
+        return self.timerecords.aggregate(models.Sum('hours'))
+
+    def __str__(self):
+        if self.parent_project:
+            return '{0.parent_project.initials}-{0.initials} - {0.name}'.format(self)
+        return '{} - {}'.format(self.initials, self.name)
