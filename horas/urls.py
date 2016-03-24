f0ff219b9ff1e9fd94ac84bfa2891b86508bed03
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-03-24 11:43:45 +0100

    Updated urls

--- horas/urls.py
+++ horas/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 import timesheets.urls
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'horas.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -11,5 +13,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'horas.views.home', name='home'),
-    url(r'^timesheets$', include(timesheets.urls)),
+
+    url(r'^timesheets/', include(timesheets.urls, namespace='timesheets')),
+
+    url(r'^login$', login),
+    url(r'^logout$', logout, name='logout'),
 )
--- timesheets/urls.py
+++ timesheets/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'timesheets.views.home', name='home'),
+    url(r'^by_employee/(?P<employee>\w{1,4})/$', 'timesheets.views.timesheet', name='timesheet_by_employee'),
 )
